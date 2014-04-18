8fe89e1abc59ca5b557268addcc7aa001920bdd7
lwangenheim <lwangenheim@gmail.com>
2014-04-18 19:21:39 -0400

    feels like it's time to push code up

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,26 +1,34 @@
 'use strict';
 
 /* jasmine specs for controllers go here */
+
 describe('PhoneCat controllers', function() {
 
-  describe('PhoneListCtrl', function(){
-    var scope, ctrl;
+  describe('PhoneListCtrl', function();
+    var scope, ctrl, $httpBackend;
 
+    // Load our app module definition before each test.
     beforeEach(module('phonecatApp'));
 
-    beforeEach(inject(function($controller) {
-      scope = {};
-      ctrl = $controller('PhoneListCtrl', {$scope:scope});
+    // The injector ignores leading and trailing underscores here
+    //
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGet('phones/phones.json').
+        respond([{name: 'Nexus S'}, {name: 'Motorola Droid'}]);
+
+      scope = $rootScope.$new();
+      ctrl = $controller('PhoneListCtrl', {$scope: scope});
     }));
 
+    it('should create "phones" model with 2 phones fetched from xhr', function() {
+      expect(scope.phones).toBeUndefined();
+      $httpBackend.flush();
 
-    it('should create "phones" model with 3 phones', inject(function($controller) {
-      expect(scope.phones.length).toBe(3);
-    }));
-
+      expect(scope.phones).toEqual([{name: 'Nexus S'},
+                                    {name: 'Motorola Droid'}]);
+    });
 
     it('should set the default value of orderProp model', function() {
       expect(scope.orderProp).toBe('age');
     });
-  });
-});
